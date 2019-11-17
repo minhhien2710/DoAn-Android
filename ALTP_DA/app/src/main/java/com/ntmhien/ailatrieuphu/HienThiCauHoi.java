0749cb1d473b8f486f844f394e5169290e213c22
Nguyễn Thành Minh Hiến <minhhien2710@users.noreply.github.com>
2019-11-17 23:18:08 +0700

    Json Cau hỏi + giao diện hiển thị câu hỏi

---
+++ ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/HienThiCauHoi.java
@@ -0,0 +1,112 @@
+package com.ntmhien.ailatrieuphu;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HienThiCauHoi extends AppCompatActivity {
+    QuanLiCauHoi quanLiCauHoi;
+
+    TextView txtSttCau;
+    TextView txtCauHoi;
+    Button btnPanA;
+    Button btnPanB;
+    Button btnPanC;
+    Button btnPanD;
+
+    int stt=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hien_thi_cau_hoi);
+
+        Log.v("Log", GetContentDataQuestion());
+
+        try {
+            quanLiCauHoi = QuanLiCauHoi.CreateByJSON(GetContentDataQuestion());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        txtSttCau = findViewById(R.id.txtSttCau);
+        txtCauHoi = findViewById(R.id.txtCauHoi);
+        btnPanA = findViewById(R.id.btnPhuongAnA);
+        btnPanB = findViewById(R.id.btnPhuongAnB);
+        btnPanC = findViewById(R.id.btnPhuongAnC);
+        btnPanD = findViewById(R.id.btnPhuongAnD);
+
+        OnNext(null);
+    }
+
+    public void OnNext(View view){
+        if(quanLiCauHoi.hasNext()){
+            resetUI(quanLiCauHoi.next());
+        }
+    }
+
+    public void resetUI(CauHoi cauHoi){
+        if(stt<=10)
+        {
+            txtSttCau.setText(""+(stt++));
+            txtCauHoi.setText(cauHoi.getCauhoi());
+            btnPanA.setText(btnPanA.getText()+" "+cauHoi.getPhuonganA());
+            btnPanB.setText(btnPanB.getText()+" "+cauHoi.getPhuonganB());
+            btnPanC.setText(btnPanC.getText()+" "+cauHoi.getPhuonganC());
+            btnPanD.setText(btnPanD.getText()+" "+cauHoi.getPhuonganD());
+        }
+        else
+        {
+            AlertDialog.Builder b=new AlertDialog.Builder(HienThiCauHoi.this);
+            b.setTitle("Hoàn thành lượt chơi");
+            b.setMessage("Bạn có muốn thoát?");
+            b.setPositiveButton("Thoát", new DialogInterface. OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    finish();
+                }});b.setNegativeButton("Chơi lại", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                finish();
+                startActivity(getIntent());
+            }
+        });
+            b.create().show();
+        }
+    }
+
+    public String GetContentDataQuestion(){
+        InputStream inputStream = getResources().openRawResource(R.raw.cauhoi);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int i;
+        try {
+            i = inputStream.read();
+            while (i != -1)
+            {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return byteArrayOutputStream.toString();
+    }
+}
--- ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/MainActivity.java
+++ ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/MainActivity.java
@@ -2,13 +2,27 @@ package com.ntmhien.ailatrieuphu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonDangNhap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonDangNhap = (Button) findViewById(R.id.btnDangNhap);
+        buttonDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HienThiCauHoi.class);
+                startActivity(intent);
+            }
+        });
     }
 }
