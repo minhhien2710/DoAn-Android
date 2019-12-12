5e64fdd9a38cd6e990f82d48c9992187b5b1962a
Cao Hiếu <CaoHieu98@users.noreply.github.com>
2019-12-12 16:01:53 +0700

    Giao Diện Ql tài khoản

---
+++ ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/QL_TaiKhoan.java
@@ -0,0 +1,14 @@
+package com.ntmhien.ailatrieuphu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QL_TaiKhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ql__tai_khoan);
+    }
+}
