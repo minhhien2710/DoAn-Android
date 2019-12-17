a7bf9b5a84507938ad9e37a5e435e7b32f1caa4e
Cao Hiếu <CaoHieu98@users.noreply.github.com>
2019-12-17 23:01:15 +0700

    LinhTinh

--- ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/MenuActivity.java
+++ ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/MenuActivity.java
@@ -2,6 +2,7 @@ package com.ntmhien.ailatrieuphu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,5 +24,26 @@ public class MenuActivity extends AppCompatActivity {
 
         musicManager = new MusicManager();
         musicManager.setNhacBatDauGame(MenuActivity.this);
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnProfile:
+                Intent intent = new Intent(MenuActivity.this, QL_TaiKhoan.class);
+                startActivity(intent);
+                break;
+            case R.id.btnHistory:
+                Intent intent1 = new Intent(MenuActivity.this, LichSuChoi.class);
+                startActivity(intent1);
+                break;
+            case R.id.btnBXH:
+                Intent intent2 = new Intent(MenuActivity.this, BXH.class);
+                startActivity(intent2);
+            case R.id.btnBuyCredit:
+                Intent intent3 = new Intent(MenuActivity.this, MuaCredit.class);
+                startActivity(intent3);
+                break;
+            default:
+                break;
+        }
     }
 }
