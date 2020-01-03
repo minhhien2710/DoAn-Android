445437718d2df0d9084e6bdb9480afbc6cd6e35e
Cao Hiếu <CaoHieu98@users.noreply.github.com>
2020-01-03 17:51:11 +0700

    Đăng nhập/đăng xuất GG và hiển thị thông tin lên ql tài khoản

--- ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/activity/MainActivity.java
+++ ALTP_DA/app/src/main/java/com/ntmhien/ailatrieuphu/activity/MainActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.ntmhien.ailatrieuphu.music.MusicManager;
 import com.ntmhien.ailatrieuphu.R;
 
@@ -14,21 +22,64 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonDangNhap;
     MusicManager musicManager;
+    SignInButton sigin;
+    int RC_SIGN_IN = 0;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonDangNhap = (Button) findViewById(R.id.btnDangNhap);
-        buttonDangNhap.setOnClickListener(new View.OnClickListener() {
+        sigin = findViewById(R.id.sign_in_button);
+        sigin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                    // ...
+                }
             }
         });
-        musicManager = new MusicManager();
-        musicManager.setNhacNen(MainActivity.this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+
+            // Signed in successfully, show authenticated UI.
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Intent intent = new Intent(MainActivity.this, QL_TaiKhoan.class);
+            startActivity(intent);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.v("Erro", "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 }
